6628e217ab0fa0b72bc47ec91cd8915bc4e23734
Manuuzzz <emmanuel.lamon@gmail.com>
2018-11-08 15:14:35 +0100

    added post count in the loop

--- home.php
+++ home.php
@@ -19,16 +19,23 @@ $args = array( 'category_name' => 'newsfr','lang' => 'fr');}
 if ( $current == 'en'){
 $args = array( 'category_name' => 'newsen','lang' => 'en');}
 $query = new WP_Query( $args ); 
+$count = $query->post_count;
+
+
 if ($query ->have_posts() ) : while ( $query ->have_posts() ) : $query ->the_post(); 
+
+
 echo '<div class="flex-news-container">';
 
 if(  has_post_thumbnail()) :
-    
+    echo '<div class="zoom-container">';
    echo '<div class="flex-thumbnail">';
     the_post_thumbnail();
-    echo '<div class="textonimage">';
-    the_post(); 
-    echo '</div></div>';
+    echo '<div class="textonimage-container">';
+   echo '<div class="textonimage">';
+    the_title(); 
+    echo '</div></div></div></div>';
+
 endif;
 
 echo '</div>';
